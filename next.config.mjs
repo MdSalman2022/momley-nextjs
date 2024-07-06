7e42c97421e31c9469b88a2339dfb2b1a8dc037d
MdSalman2022 <salman15-4523@diu.edu.bd>
2024-07-06 13:43:03 +0600

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    VITE_SERVER_URL: "http://localhost:5000",
-    VITE_IMGBB_KEY: "e9ee41ec2bd1b26ca469c791ef1a12c2",
+    VITE_SERVER_URL: process.env.VITE_SERVER_URL,
+    VITE_IMGBB_KEY: process.env.VITE_IMGBB_KEY,
   },
   images: {
     remotePatterns: [
